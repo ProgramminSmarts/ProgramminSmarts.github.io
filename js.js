1104df34f36c9ac310fbc4c63e4b09760b213384
ProgramminSmarts <hsbosskid@gmail.com>
2017-11-13 19:35:20 -0500

    Updated Hershey Clicker from Code Pen with current code

--- js.js
+++ js.js
@@ -1,26 +1,78 @@
+var chest = document.getElementById("chest").innerHTML;
+
 var score = 0;
 var cpc = 1;
 var cps = 0;
+
 var factoryCPS = 1;
 var factoryCost = 60;
+
 var workerCPS = 4;
 var workerCost= 225;
+
 var numbFactories = 0;
 var numbWorkers = 0;
+
 var addCPCCost = 650;
 
+var treasureCost = 500;
+var doubleChance = 99;
+var chancePercentage = 1;
+
+var chestChance = 0;
+
 function warning() {
-  alert(" By clicking 'Ok' or choosing to exit this message, you agree to the following terms:\nI am not responsible for this game impeding in your work or learning, and the intent of this game was not to distract you either. Please play on your discretion!\n              -Harshul ");
+  alert("By clicking 'Ok' or choosing to exit this message, you agree to the following terms:\nI am not responsible for this game impeding in your work or learning, and the intent of this game was not to distract you either. Please play on your discretion!\n-Harshul ");
 }
 
 function spacebar(e) {
   if(e.keyCode == 32) {
-    score += cpc;
-    update();
+    clicked();
   }
+}
+function clicked () {
+  var randNumb = Math.random()*100+1;
+  if (randNumb>doubleChance) {
+    score = score+(cpc*2);
+  }
+  else {
+    score+=cpc;
+  }
+
+  update();
+
 }
 onkeyup = spacebar;
 
+function chest() {
+  document.createElement("div");
+  div.style.width = "100px";
+  div.style.height = "150px";
+  div.style.background = "blue";
+  div.style.left = "300px";
+  div.style.top = "400px";
+}
+
+function buyTreasure () {
+  
+  if (score>=treasureCost) {
+    
+    if(doubleChance>65) {
+      score -= treasureCost;
+      doubleChance -= 1;
+      chancePercentage += 1;
+      treasureCost += Math.floor(0.055*treasureCost);
+      document.getElementById("chanceCost").innerHTML = "Cost: " + treasureCost + "  Chocolate Chips";
+      document.getElementById("currentChance").innerHTML = "Current Chance: " + chancePercentage +"%";
+    }
+    else {
+      alert("You have the reached the max percentage, you can no longer buy any more of this treasure.");
+      document.getElementById("currentChance").innerHTML = "Current Chance: 35% (Max)";
+      document.getElementById("chanceCost").innerHTML = " ";
+    }
+    
+  }
+}
 function buyFactory () {
   if (score>=factoryCost) {
     cps += factoryCPS;
@@ -50,16 +102,7 @@ function update () {
   document.getElementById('cpcDisp').innerHTML = "CPC: " + cpc;
 }
 
-function clicked () {
-  var randNumb = (Math.random()+1)*100;
-  if (randNumb>99) {
-    score = score+(cpc*2);
-  }
-  else {
-    score+=cpc;
-  }
-  update();
-}
+
 
 function addingCPS () {
   score += cps;
@@ -82,4 +125,20 @@ function openTab (tabName) {
     x[i].style.display = "none";
   }
   document.getElementById(tabName).style.display = "block";
+}
+function info() {
+  var q1 = prompt("What do you need help with? \n    Manual\n    Shop  \n   Upgrades\n   Treasures");
+  if (q1 == "treasures" || q1 == "Treasures") {
+    alert("Treasures are special abilities that may cost a lot, but are worth. These are more long-term upgrades.\n  The first treasure increases the chance you get double the amount of chocolate chips you would normally get in a click. The max percentage you can have is 35%.");
+  }
+  else if (q1 == "Manual" || q1 == "manual") {
+    alert("How to play:\n   To get points, or chocolate chips, you have to click the Hershey Bar. It displays your score with 'Score: Chocolate Chips'. Underneath that, it says CPS and CPC. They stand for Chips Per Second and Chips Per Click. The CPS means that you get a certain amount of chips every second. The CPC means the amounts of chips you get everytime you click. You can upgrade both of these with the shop.");
+  }
+  else if (q1 == null) {
+    return;
+  }
+  else {
+    alert("Please enter one of the options given for more information about the topic.");
+    info();
+  }
 }
